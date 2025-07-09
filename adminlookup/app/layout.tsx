20b03b0fc6e0890d669eb0d734d1ea4664b9af03
Elkana Kipruto <allanyegon443@gmail.com>
2025-07-09 13:52:33 +0300

    Font geist removal

--- adminlookup/app/layout.tsx
+++ adminlookup/app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from "next";
-import { Inter, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Inter({
-  variable: "--font-inter-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
